9c5879214827c9e3ec2adce165bbc0b729f3a0e8
Amigos <Amigos@Air-de-mba.fibertel.com.ar>
2022-12-09 15:04:30 -0300

    Se agrega el proyecto

---
+++ academlo.page/main.js
@@ -0,0 +1,43 @@
+const items = [
+    {
+      id: 1,
+      name: 'Hoodies',
+      price: 14.00,
+      image: 'assets/images/featured1.png',
+      category: 'hoodies',
+      quantity: 10
+    },
+    {
+      id: 2,
+      name: 'Shirts',
+      price: 24.00,
+      image: 'assets/images/featured2.png',
+      category: 'shirts',
+      quantity: 15
+    },
+    {
+      id: 3,
+      name: 'Sweatshirts',
+      price: 24.00,
+      image: 'assets/images/featured3.png',
+      category: 'shirts',
+      quantity: 20
+    }
+  ]
+
+
+  const btnCart = document.querySelector('.nav_btns')
+
+  const containerCartProducts = document.querySelector('.container_cart_products')
+
+
+  btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle
+    ('.hidden_cart')
+  })
+
+
+
+
+
+  
